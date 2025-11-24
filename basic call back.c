f2a1659e2eb5cdb116b9f4aae4e68c55322357ec
Chandan raj r <nadnahc1566@gmail.com>
2025-11-24 07:44:59 +0530

    Add files via upload

---
+++ basic call back.c
@@ -0,0 +1,15 @@
+void sayhello(){
+     printf("HELLO,STUDENT! YOU JUST CALLED ME BACK!\n");
+
+}
+
+void studentWork(void(*callbackfunc)()){
+    printf("STUDENT IS DOING HOMEWORK..\n");
+
+    callbackfunc();
+}
+int main(){
+    studentWork(sayhello);
+    return 0;
+
+}
